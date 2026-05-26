a5412ac1d3ffd56c90446807d381a94543dce40e
hollyursule_ <hollykalisa@gmail.com>
2026-05-26 09:46:53 -0700

    my first commit on first project

---
+++ stage1/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 class= "logo logo--top left">
+    <img 
+  src="./assets/logo.png.png" 
+ class="w-50 h-20"
+
+    </h1>
+
+
+   <h2 class="text-2xl font-bold text-center text-underline text-italic text-blue-500 mt-8">
+    SOS BLOG
+  </h2>
+
+  <p class="text-4xl text-center text-gray-600 mt-4">
+     Discover fresh ideas, helpful tips, and exciting stories from different
+      topics around the world. Our blog is here to inspire, inform, and keep
+      you updated with new content every week.
+  </p>
+</template>
+
+<style scoped></style>
